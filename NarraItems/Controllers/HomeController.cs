ee528e3fd4fa7eda15a16074249b9433d988467c
unknown <chavezengr@gmail.com>
2018-08-05 20:13:17 +0800

    narra project

---
+++ NarraItems/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NarraItems.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(){
+            //return typeof(Controller).Assembly.GetName().Version.ToString();
+            return "Hello Joeram Chavez Welcome to MVC........8===D";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NarraItems/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NarraItems.MvcApplication" Language="C#" %>
